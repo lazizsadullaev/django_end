5fa99724b3b0ce33eaf5b807babe6201e583f40a
student <student@intranet.proweb.uz>
2023-11-25 17:24:44 +0500

    finished project

---
+++ web_site/admin.py
@@ -0,0 +1,38 @@
+# def perimetre_square(side_a: float):
+#     print(perimetre_square(side_a=4.5))
+#
+#     def area_square(side_a: float):
+#         return side_a * side_a
+#
+#     print(area_square(side_a=5))
+#
+#     def perimetre_rectangle(side_a, side_b)
+#
+#     print(perimetre_rectangle(side_a=5, side_b=10))
+#     print(perimetre_rectangle(5, 10))
+#     return side_a * 4
+
+# def number_avg(*numbers):
+#       return sum(numbers)/ len(numbers)
+# print(numbers_avg(25, 9, 1, 30, 12, 13, 7))
+
+# def avg_marks(**marks):
+#     marks_values
+#     return sum(marks) / len(marks)
+#
+#
+# print(avg_marks(math=3, phylosophy=4, programing=5, bidgate=3, english=4))
+
+# 200-200
+
+
+
+
+
+
+
+
+
+
+
+
